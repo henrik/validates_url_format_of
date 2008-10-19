956e2e3e1ab86ec5297bd6809a53bf37165a8724
Henrik N <henrik@nyh.se>
2008-10-19 16:27:21 +0200

    Correct comment.

--- test/validates_url_format_of_test.rb
+++ test/validates_url_format_of_test.rb
@@ -44,7 +44,7 @@ class ValidatesUrlFormatOfTest < Test::Unit::TestCase
       'http://www.sub.example.com/page.html?foo=bar&baz=%23#anchor',
       'http://user:pass@example.com',
       'http://example.com/~user',
-      'http://example.co',  # not a real TLD, but we're fine with anything of 2+ chars
+      'http://example.co',  # Not a real TLD, but we're fine with anything of 2-6 chars
       'http://example.museum',
       'http://1.0.255.249',
       'http://1.2.3.4:80',
