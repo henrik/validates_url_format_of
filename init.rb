efd18586f9e5c2a70c0487afb29b178f0f2e7d42
Henrik N <henrik@nyh.se>
2008-10-19 15:39:57 +0200

    Make regexp more readable with /x, inspired by http://code.djangoproject.com/attachment/ticket/9202/field_url_re.diff.

--- init.rb
+++ init.rb
@@ -1,8 +1,15 @@
 module ValidatesUrlFormatOf
-  HOSTNAME  = /[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,6}/i
   IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
-  IPv4 = /(#{IPv4_PART}\.){3}#{IPv4_PART}/
-  REGEXP = %r{\Ahttps?://([^\s:@]+:[^\s:@]+@)?(#{HOSTNAME}|#{IPv4})(:\d{1,5})?([/?].*)?\Z}i  
+  REGEXP = %r{
+    \A
+    https?://                                  # http:// or https://
+    ([^\s:@]+:[^\s:@]+@)?                      # optional username:pw@
+    ( [a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,6} |  # domain...
+        #{IPv4_PART}(\.#{IPv4_PART}){3} )      # or IPv4
+    (:\d{1,5})?                                # optional port
+    ([/?].*)?                                  # optional /whatever or ?whatever
+    \Z
+  }ix
   
   def validates_url_format_of(*attr_names)
     options = { :message => 'does not appear to be valid',
