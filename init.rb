641a02c47123d5f9b6314d0aab275350d460be54
Billy Reisinger <billy.reisinger@gmail.com>
2010-04-02 13:40:10 -0500

    Bugfix: punycoded domains with www in front were marked as invalid.

--- init.rb
+++ init.rb
@@ -2,12 +2,12 @@ module ValidatesUrlFormatOf
   IPv4_PART = /\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]/  # 0-255
   REGEXP = %r{
     \A
-    https?://                                        # http:// or https://
-    ([^\s:@]+:[^\s:@]*@)?                            # optional username:pw@
-    ( (xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
-        #{IPv4_PART}(\.#{IPv4_PART}){3} )            # or IPv4
-    (:\d{1,5})?                                      # optional port
-    ([/?]\S*)?                                       # optional /whatever or ?whatever
+    https?://                                                    # http:// or https://
+    ([^\s:@]+:[^\s:@]*@)?                                        # optional username:pw@
+    ( (([^\W_]+\.)?xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
+        #{IPv4_PART}(\.#{IPv4_PART}){3} )                        # or IPv4
+    (:\d{1,5})?                                                  # optional port
+    ([/?]\S*)?                                                   # optional /whatever or ?whatever
     \Z
   }iux
 
